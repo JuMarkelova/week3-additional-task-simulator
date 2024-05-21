4cdd0ad0bd4600cd9ae3d41a35f3f3c2b65877d5
Iuliia M <89371934+JuMarkelova@users.noreply.github.com>
2024-05-21 21:54:47 +0300

    fix names, events 10/10

--- src/main/java/cy/markelova/simulator/_main/_Main.java
+++ src/main/java/cy/markelova/simulator/_main/_Main.java
@@ -1,13 +1,12 @@
 package cy.markelova.simulator._main;
 
 import cy.markelova.simulator.entity.Octopus;
-
-import static cy.markelova.simulator.util.Events.runLife;
+import cy.markelova.simulator.util.Events;
 
 public class _Main {
 
     public static void main(String[] args) {
         Octopus octopus = new Octopus();
-        runLife(octopus);
+        Events.runLife(octopus);
     }
 }
--- src/main/java/cy/markelova/simulator/util/Events.java
+++ src/main/java/cy/markelova/simulator/util/Events.java
@@ -2,38 +2,51 @@ package cy.markelova.simulator.util;
 
 import cy.markelova.simulator.entity.Octopus;
 
+@SuppressWarnings("ManualMinMaxCalculation")
 public class Events {
 
-    private static void birthOfOctopus() {
+    public static void runLife(Octopus octopus) {
+        giveBirthOctopus();
+        while (octopus.getIsAlive()) {
+            int randomNumber = createRandomNumber();
+            if (randomNumber > 0 && randomNumber <= 15) eatFish(octopus);
+            if (randomNumber > 15 && randomNumber <= 50) surroundOctopusBySparks(octopus);
+            if (randomNumber > 50 && randomNumber <= 58) swim(octopus);
+            if (randomNumber > 58 && randomNumber <= 69) sleep(octopus);
+            if (randomNumber > 69 && randomNumber <= 80 && octopus.getIsInjured()) growTentacles(octopus);
+            if (randomNumber > 80 && randomNumber <= 90) attackOctopusByHuman(octopus);
+            if (randomNumber > 90 && randomNumber <= 100) eatLobster(octopus);
+            if (octopus.getEnergy() == 0) octopus.setHealthPoints(lowHealthPoints(octopus, 20));
+            if (octopus.getHealthPoint() == 0) {
+                octopus.setIsAlive(false);
+                printLastMessage();
+            }
+            System.out.println("Текущее состояние осьминога. Здоровье: " + octopus.getHealthPoint() + " и " + "энергия: " + octopus.getEnergy());
+            try {
+                //noinspection BusyWait
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void giveBirthOctopus() {
         String introduction = """
                 Сегодня родилась маленькая особь осьминога!
                 Это самка. Жизнь осьминога трудна, опасна.
-                И состовляет не более двух лет.
+                И составляет не более двух лет.
                 """;
         System.out.println(introduction);
     }
 
-    private static void eatingFish(Octopus octopus) {
-        octopus.setEnergy(highEnergy(octopus, 5));
+    private static void eatFish(Octopus octopus) {
+        octopus.setEnergy(upEnergy(octopus, 5));
         String message = "Осьминог поела рыбок! Силы восстановлены.";
         System.out.println(message);
     }
 
-    private static void swim(Octopus octopus) {
-        octopus.setEnergy(lowEnergy(octopus, 8));
-        String message = """
-                Какой отличный день, вокруг никаких опасностей, можно вдоволь поплавать!""";
-        System.out.println(message);
-    }
-
-    private static void sleep(Octopus octopus) {
-        octopus.setEnergy(highEnergy(octopus, 10));
-        octopus.setHealthPoints(highHealthPoints(octopus, 10));
-        String message = "Удалось спокойно поспать и набраться сил.";
-        System.out.println(message);
-    }
-
-    private static void thereAreOctopusEnemyAround(Octopus octopus) {
+    private static void surroundOctopusBySparks(Octopus octopus) {
         octopus.setEnergy(lowEnergy(octopus, 40));
         String message = "Вокруг плавают усатые кошачьи акулы. Чтобы выжить нужно спрятаться, ушло столько сил.";
         System.out.println(message);
@@ -43,8 +56,21 @@ public class Events {
         } else hidUnsuccessfully(octopus);
     }
 
+    private static void swim(Octopus octopus) {
+        octopus.setEnergy(lowEnergy(octopus, 8));
+        String message = "Какой отличный день, вокруг никаких опасностей, можно вдоволь поплавать!";
+        System.out.println(message);
+    }
+
+    private static void sleep(Octopus octopus) {
+        octopus.setEnergy(upEnergy(octopus, 10));
+        octopus.setHealthPoints(upHealthPoints(octopus, 10));
+        String message = "Удалось спокойно поспать и набраться сил.";
+        System.out.println(message);
+    }
+
     private static void hidSuccessfully(Octopus octopus) {
-        octopus.setEnergy(highEnergy(octopus, 5));
+        octopus.setEnergy(upEnergy(octopus, 5));
         String message = "Осьминогу удалось укрыться. Она чувствует облегчение.";
         System.out.println(message);
     }
@@ -57,7 +83,7 @@ public class Events {
         System.out.println(message);
     }
 
-    private static void attackedByHuman(Octopus octopus) {
+    private static void attackOctopusByHuman(Octopus octopus) {
         octopus.setHealthPoints(lowHealthPoints(octopus, 25));
         octopus.setEnergy(lowEnergy(octopus, 10));
         String message = "О нет! Человек. Он не так страшен как акула, но в случае неудачи осьминога зажарят на гриле.";
@@ -65,13 +91,13 @@ public class Events {
     }
 
     private static void eatLobster(Octopus octopus) {
-        octopus.setEnergy(highEnergy(octopus, 15));
-        String message = "Повезло, попался омар, что является большой редкостью!";
+        octopus.setEnergy(upEnergy(octopus, 15));
+        String message = "Повезло, на обед попался омар, что является большой редкостью!";
         System.out.println(message);
     }
 
     private static void growTentacles(Octopus octopus) {
-        octopus.setHealthPoints(highHealthPoints(octopus, 5));
+        octopus.setHealthPoints(upHealthPoints(octopus, 5));
         octopus.setIsInjured(false);
         String message = "Начали отрастать новые щупальца!";
         System.out.println(message);
@@ -79,74 +105,30 @@ public class Events {
 
     private static int lowEnergy(Octopus octopus, int energy) {
         int newEnergy = octopus.getEnergy() - energy;
-        if (newEnergy <= 0) {
-            return 0;
-        } else return newEnergy;
+        return newEnergy <= 0 ? 0 : newEnergy;
     }
 
-    private static int highEnergy(Octopus octopus, int energy) {
+    private static int upEnergy(Octopus octopus, int energy) {
         int newEnergy = octopus.getEnergy() + energy;
-        if (newEnergy > 100) {
-            return 100;
-        } else return newEnergy;
+        return newEnergy > 100 ? 100 : newEnergy;
     }
 
     private static int lowHealthPoints(Octopus octopus, int points) {
         int newHealthPoints = octopus.getHealthPoint() - points;
-        if (newHealthPoints <= 0) {
-            return 0;
-        } else return newHealthPoints;
+        return newHealthPoints <= 0 ? 0 : newHealthPoints;
     }
 
-    private static int highHealthPoints(Octopus octopus, int points) {
+    private static int upHealthPoints(Octopus octopus, int points) {
         int newHealthPoints = octopus.getHealthPoint() + points;
-        if (newHealthPoints > 100) {
-            return 100;
-        } else return newHealthPoints;
-    }
-
-    private static void hasEnergy(Octopus octopus) {
-        if (octopus.getEnergy() == 0) {
-            octopus.setHealthPoints(lowHealthPoints(octopus, 20));
-        }
-    }
-
-    private static void isAlive(Octopus octopus) {
-        if (octopus.getHealthPoint() == 0) {
-            octopus.setIsAlive(false);
-            dieOctopus();
-        }
+        return newHealthPoints > 100 ? 100 : newHealthPoints;
     }
 
     private static int createRandomNumber() {
         return (int) (Math.random() * 100);
     }
 
-    private static void dieOctopus() {
-        String message = """
-                Прожив свою короткую, но насыщенную жизнь осьминог умерла.""";
+    private static void printLastMessage() {
+        String message = "Прожив свою короткую, но насыщенную жизнь осьминог умерла.";
         System.out.println(message);
-    }
-
-    public static void runLife(Octopus octopus) {
-        birthOfOctopus();
-        while (octopus.getIsAlive()) {
-            int randomNumber = createRandomNumber();
-            if (randomNumber <= 15) eatingFish(octopus);
-            if (randomNumber <= 50) thereAreOctopusEnemyAround(octopus);
-            if (randomNumber <= 58) swim(octopus);
-            if (randomNumber <= 69) sleep(octopus);
-            if (randomNumber <= 80 && octopus.getIsInjured()) growTentacles(octopus);
-            if (randomNumber <= 90) attackedByHuman(octopus);
-            if (randomNumber <= 100) eatLobster(octopus);
-            hasEnergy(octopus);
-            isAlive(octopus);
-            System.out.println("Текущее состояние осьминога. Здоровье: " + octopus.getHealthPoint() + " и " + "энергия: " + octopus.getEnergy());
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 }
